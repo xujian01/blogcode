610923ce95433bda03cf8c05c97ad99911f6fbd6
xujian <xujian@dankegongyu.com>
2019-09-04 16:48:01 +0800

    增加机器做汤UML

--- src/buildermode/SoupProgram.java
+++ src/buildermode/SoupProgram.java
@@ -1,8 +1,8 @@
 package buildermode;
 
 public interface SoupProgram {
-    void step1();
-    void step2();
-    void step3();
-    Soup make();
+    void step1();//步骤1
+    void step2();//步骤2
+    void step3();//步骤3
+    Soup make();//做汤
 }
