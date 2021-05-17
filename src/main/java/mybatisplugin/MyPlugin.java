26280ed93a96194be2edda9edc7baa01e139acd5
徐健03 <xujian03@baijiahulian.com>
2021-05-17 20:51:24 +0800

    增加mybatis包相关代码

--- src/main/java/mybatisplugin/MyPlugin.java
+++ src/main/java/mybatisplugin/MyPlugin.java
@@ -12,14 +12,15 @@ import java.sql.Statement;
  * @author xujian
  * 2021-05-11 15:22
  **/
-@Intercepts(@Signature(type = ResultSetHandler.class,
-        method = "handleResultSets",
-        args = {Statement.class}))
+@Intercepts(@Signature(type = ResultSetHandler.class,//指定你要拦截是哪个对象
+        method = "handleResultSets",//指定你要拦截的是哪个方法
+        args = {Statement.class}))//考虑到方法重载，还需要指定参数列表
 @Slf4j
 public class MyPlugin implements Interceptor {
     @Override
     public Object intercept(Invocation invocation) throws Throwable {
         log.info("----插件拦截到handleResultSets----");
+        //实行原始的方法调用
         return invocation.proceed();
     }
 }
